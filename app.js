751f46e629367a8f6301e0b28aead5eb01bd96ae
BdsOliveira <bds.commus@gmail.com>
2022-04-28 13:01:26 -0300

    Pausing project for now

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ require('dotenv').config()
 const PORT = process.env.PORT || 5000;
 
 const { google } = require("googleapis");
-const { OAuth } = google.auth;
+const { OAuth2 } = google.auth;
 
-/* const oAuth2Client = new OAuth2({
+const oAuth2Client = new OAuth2(
+    process.env.COSTUMER_ID,
+    process.env.CONTUMER_SECRET_KEY
+);
 
-}); */
+oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN});
 
-app.get('/', (req, res) => {
-    res.status(200).send('GET Request OK');
-});
-
+const calendar = google.calendar({version: 'v3', auth: oAuth2Client});
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
